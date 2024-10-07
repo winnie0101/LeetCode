bb81c2f67b5f6cb5418e80c45f533731973e8b68
Winnie <winne52889@gmail.com>
2024-10-07 19:32:03 +0800

    majority

---
+++ Majority_Element/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        
+    }
+};
+
+int main() {
+    Solution s;
+    vector <int> nums = {3, 2, 3};
+    cout << s.majorityElement(nums) << endl;
+}
